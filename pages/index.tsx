713837caa0b0ab2f8126285056d86f7a0461e6ae
sota_masuda <sota.m777121@gmail.com>
2023-02-11 17:03:40 +0900

    made Stories from MoreStories, no use HeroPost, MoreStories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
+import Stories from '@/components/stories'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
@@ -12,8 +11,6 @@ type Props = {
 }
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -22,17 +19,7 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <AppBar />
         <Container>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 ? <Stories posts={allPosts} /> : <>No Stories</>}
         </Container>
       </Layout>
     </>
