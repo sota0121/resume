28eab7fdba436cdac3f1285d75bfc4c347dbdca1
sota_masuda <sota.m777121@gmail.com>
2023-02-18 18:49:10 +0900

    fix profile simple

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import Layout from '../components/layout'
-import Head from 'next/head'
-import AppBar from '../components/app-bar'
 
 export const Profile = () => {
   return (
-    <Layout>
-      <Head>
-        <title>{'SOTA Lab Home Page'}</title>
-      </Head>
-      <AppBar />
-      <div className="container mx-auto">
-        Profile Page is under construction ...
-      </div>
-    </Layout>
+    <div className="container mx-auto">
+      Profile Page is under construction ...
+    </div>
   )
 };
 
