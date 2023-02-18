9a05b2c2aead8369bce9fbf6762a10b46b0e562a
sota_masuda <sota.m777121@gmail.com>
2023-02-18 18:49:55 +0900

    fix app bar for xs mode

--- components/app-bar-menu.tsx
+++ components/app-bar-menu.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import { MobileNavProps } from '@material-tailwind/react';
 
 type MenuProps = {
   title: string;
   href: string;
   description: string;
+  onClick?: () => void;
 };
 
 const MenuList = [
@@ -20,18 +22,60 @@ const MenuList = [
   }
 ]
 
-export const AppBarMenu: React.FC = ( props: MenuProps ) => {
+const AppBarMenu: React.FC<MenuProps> = ( props: MenuProps ) => {
   return (
     <li>
       {/* <Link href={props.href} className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-gray-800 md:p-0 dark:text-white" aria-current="page"> */}
-      <Link href={props.href}>
+      <Link href={props.href} className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0">
         {props.title}
       </Link>
     </li>
   )
 }
 
+const MobileAppBarMenu: React.FC<MenuProps> = ( props: MenuProps ) => {
+  return (
+    <li>
+      <Link className="text-xl font-medium my-4" href={props.href} onClick={props.onClick}>
+        {props.title}
+      </Link>
+    </li>
+  )
+};
+
+type MobileAppBarMenuListProps = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MobileAppBarMenuList: React.FC<MobileAppBarMenuListProps> = ( props: MobileAppBarMenuListProps) => {
+  // State
+  const { isOpen, setIsOpen } = props;
+
+  // Event Handler
+  const hundleMenuClick = () => {
+    setTimeout(() => {setIsOpen(!isOpen)}, 100);
+  }
+
+  // Render
+  return (
+    // <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${isOpen ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+    <div>
+    {/* <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform -translate-x-0 transition-transform duration-300 ease-in-out filter drop-shadow-md `}> */}
+      <ul className='flex flex-row p-2 mt-2 md:flex-row md:space-x-4 md:mt-0 xs:flex-row xs:space-x-4'>
+        <div className="flex flex-row ml-4">
+          {MenuList.map((menu, index) => {
+            return <MobileAppBarMenu key={index} onClick={hundleMenuClick} {...menu} />
+          })}
+        </div>
+      </ul>
+    </div>
+  )
+
+};
+
 export const AppBarMenuList: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <>
       {/** humburger menu (todo) */}
@@ -40,8 +84,9 @@ export const AppBarMenuList: React.FC = () => {
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button> */}
       <div className="w-full md:block md:w-auto" id="navbar-default">
+        {/* <MobileAppBarMenuList isOpen={isOpen} setIsOpen={setIsOpen} /> */}
         {/* <ul className="flex flex-col p-2 mt-2 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white"> */}
-        <ul className='flex flex-col p-2 mt-2 md:flex-row md:space-x-4 md:mt-0 xs:flex-row xs:space-x-2'>
+        <ul className='flex flex-row justify-end p-2 mt-2 md:flex-row md:space-x-4 md:mt-0 xs:flex-row xs:space-x-4 xs:mt-0'>
           {MenuList.map((menu, index) => {
             return <AppBarMenu key={index} {...menu} />
           })}
