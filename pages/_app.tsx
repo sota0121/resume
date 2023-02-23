a9d87fb4e78ec7ecade995b38364ba50699f28b6
sota_masuda <sota.m777121@gmail.com>
2023-02-23 18:18:21 +0900

    fix warnint of A title element received an array with more than 1 element as children

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import '../styles/index.css'
 import Layout from '../components/layout'
 import Head from 'next/head'
 import AppBar from '../components/app-bar'
+import { BASE_PAGE_TITLE } from '@/lib/constants'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
         <Head>
-          <title>{'SOTA Lab Home Page'}</title>
+          <title>{BASE_PAGE_TITLE}</title>
         </Head>
         <AppBar />
         <Component {...pageProps} />
