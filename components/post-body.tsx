841d2965e2f981e27a5ccf0242e8820ee42fec5d
sota_masuda <sota.m777121@gmail.com>
2023-02-23 20:20:16 +0900

    introduce highlight.js but, that doesn't work

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,10 +1,55 @@
+import { useEffect, useRef } from 'react'
 import markdownStyles from './markdown-styles.module.css'
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import typescript from 'highlight.js/lib/languages/typescript'
+import bash from 'highlight.js/lib/languages/bash'
+import css from 'highlight.js/lib/languages/css'
+import json from 'highlight.js/lib/languages/json'
+import xml from 'highlight.js/lib/languages/xml'
+import yaml from 'highlight.js/lib/languages/yaml'
+import go from 'highlight.js/lib/languages/go'
+import python from 'highlight.js/lib/languages/python'
+import java from 'highlight.js/lib/languages/java'
+import php from 'highlight.js/lib/languages/php'
+import ruby from 'highlight.js/lib/languages/ruby'
+import rust from 'highlight.js/lib/languages/rust'
+import scala from 'highlight.js/lib/languages/scala'
+import swift from 'highlight.js/lib/languages/swift'
+import kotlin from 'highlight.js/lib/languages/kotlin'
+import csharp from 'highlight.js/lib/languages/csharp'
+import cpp from 'highlight.js/lib/languages/cpp'
+import c from 'highlight.js/lib/languages/c'
+
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('typescript', typescript)
+hljs.registerLanguage('bash', bash)
+hljs.registerLanguage('css', css)
+hljs.registerLanguage('json', json)
+hljs.registerLanguage('xml', xml)
+hljs.registerLanguage('yaml', yaml)
+hljs.registerLanguage('go', go)
+hljs.registerLanguage('python', python)
+hljs.registerLanguage('java', java)
+hljs.registerLanguage('php', php)
+hljs.registerLanguage('ruby', ruby)
+hljs.registerLanguage('rust', rust)
+hljs.registerLanguage('scala', scala)
+hljs.registerLanguage('swift', swift)
+hljs.registerLanguage('kotlin', kotlin)
+hljs.registerLanguage('csharp', csharp)
+hljs.registerLanguage('cpp', cpp)
+hljs.registerLanguage('c', c)
 
 type Props = {
   content: string
 }
 
 const PostBody = ({ content }: Props) => {
+  useEffect(() => {
+    hljs.initHighlightingOnLoad()
+  })
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
