aaec96d1ab061aae0a2103b8fe11935dc06d2250
sota_masuda <sota.m777121@gmail.com>
2023-02-23 19:39:47 +0900

    introduce remark-highlight

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,22 +1,35 @@
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import rehypeSanitize from 'rehype-sanitize'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
+import rehypeHighlight from 'rehype-highlight'
 import rehypeStringify from 'rehype-stringify'
 
 
 // ref: https://github.com/remarkjs/remark#example-turning-markdown-into-html
 // ref: https://vivliostyle.github.io/vivliostyle_doc/ja/vivliostyle-user-group-vol2/spring-raining/index.html
+// rehype-highlight: https://github.com/rehypejs/rehype-highlight
 
 export default async function markdownToHtml(markdown: string) {
   const vfile = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeSanitize)
-    .use(rehypeStringify)
+    .use(remarkParse) // Parse Markdown to AST
+    .use(remarkRehype) // Transform Markdown AST to HTML AST
+    .use(rehypeSanitize, { // Sanitize HTML
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        code: [
+          ...(defaultSchema.attributes?.code || []),
+          // List of allowed languages
+          ['className', 'language-js', 'language-css', 'language-md'],
+        ],
+      },
+    })
+    .use(rehypeHighlight) // Highlight code blocks with highlight.js
+    .use(rehypeStringify) // Stringify HTML AST to HTML
     .process(markdown)
 
-  console.log(vfile.toString())
+  console.log(vfile)
 
   // const result = await remark().use(html).process(markdown)
   return String(vfile)
