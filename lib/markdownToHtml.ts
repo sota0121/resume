765c68a86791c0dbfad0f1bfda67ee87dd58f99b
sota_masuda <sota.m777121@gmail.com>
2023-02-23 18:21:12 +0900

    fix blog sentence a little, introduce rehype ecosystem

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,21 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+
+
+// ref: https://github.com/remarkjs/remark#example-turning-markdown-into-html
+// ref: https://vivliostyle.github.io/vivliostyle_doc/ja/vivliostyle-user-group-vol2/spring-raining/index.html
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const vfile = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process(markdown)
+
+  // const result = await remark().use(html).process(markdown)
+  return String(vfile)
 }
